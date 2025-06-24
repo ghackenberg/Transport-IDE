cce47c69638a798e223a5151e877e76cc5d5d9c8
Dr. Georg Hackenberg <ghackenberg@gmail.com>
2025-06-24 12:07:53 +0200

    export adapters

--- modules/core/src/main/java/module-info.java
+++ modules/core/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module mbse.transport.core {
 	
 	exports io.github.ghackenberg.mbse.transport.core;
 	exports io.github.ghackenberg.mbse.transport.core.entities;
+	exports io.github.ghackenberg.mbse.transport.core.adapters;
 	exports io.github.ghackenberg.mbse.transport.core.controllers;
 	exports io.github.ghackenberg.mbse.transport.core.exporters;
 	exports io.github.ghackenberg.mbse.transport.core.exceptions;
