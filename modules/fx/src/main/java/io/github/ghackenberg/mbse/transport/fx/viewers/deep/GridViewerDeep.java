e13ffc4a3b5d20ffc246517c23b2163b52ee5e11
Dr. Georg Hackenberg <ghackenberg@gmail.com>
2025-07-15 14:22:16 +0200

    improve dynamic 3d grid resolution and painting performance

--- modules/fx/src/main/java/io/github/ghackenberg/mbse/transport/fx/viewers/deep/GridViewerDeep.java
+++ modules/fx/src/main/java/io/github/ghackenberg/mbse/transport/fx/viewers/deep/GridViewerDeep.java
@@ -1,6 +1,8 @@
 package io.github.ghackenberg.mbse.transport.fx.viewers.deep;
 
 import io.github.ghackenberg.mbse.transport.core.Model;
+import io.github.ghackenberg.mbse.transport.core.entities.Camera;
+import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -12,9 +14,19 @@ import javafx.scene.shape.TriangleMesh;
 public class GridViewerDeep extends Group {
 
     private final Model model;
+    private final Camera camera;
 
-    public GridViewerDeep(Model model) {
+    private double previousStep = 0;
+    
+    private double previousMinX = 0;
+    private double previousMinY = 0;
+
+    private double previousMaxX = 0;
+    private double previousMaxY = 0;
+
+    public GridViewerDeep(Model model, Camera camera) {
         this.model = model;
+        this.camera = camera;
 
         model.min.x.addListener(event -> updateGrid());
         model.min.y.addListener(event -> updateGrid());
@@ -23,35 +35,71 @@ public class GridViewerDeep extends Group {
         model.max.y.addListener(event -> updateGrid());
         model.max.z.addListener(event -> updateGrid());
 
+        camera.eye.x.addListener(event -> updateGrid());
+        camera.eye.y.addListener(event -> updateGrid());
+        camera.eye.z.addListener(event -> updateGrid());
+
         updateGrid();
     }
 
     private void updateGrid() {
-        getChildren().clear();
+
+        // Distance
+
+        Point3D eye = camera.eye.toPoint3D();
+        Point3D direction = camera.direction.toPoint3D();
+        Point3D center = eye.add(direction.multiply(-eye.getZ() / direction.getZ()));
+
+        System.out.println("Center = " + center.getX() + " " + center.getY() + " " + center.getZ());
+
+        double distance = center.subtract(eye).magnitude();
+
+        if (distance == 0) {
+            return;
+        }
+
+        // Step
+
+        double step = 1;
+
+        while (distance / step > 3) {
+            step *= 10;
+        }
+        while (distance / step < 3) {
+            step /= 10;
+        }
+
+        // Min / max
 
         double minX = Math.ceil(model.min.x.get() - 1.5);
         double minY = + Math.ceil(model.min.y.get() - 1.5);
 
-        double maxX = Math.floor(model.max.x.get() + 1.5);
-        double maxY = Math.floor(model.max.y.get() + 1.5);
-
-        double dX = maxX - minX;
-        double dY = maxY - minY;
-
-        double step = 1;
-
-        while (Math.min(dX / step, dY / step) > 3) {
-            step *= 10;
-        }
-        while (Math.max(dX / step, dY / step) < 3) {
-            step /= 10;
-        }
-
         minX = Math.floor(minX / step) * step;
         minY = Math.floor(minY / step) * step;
 
+        double maxX = Math.floor(model.max.x.get() + 1.5);
+        double maxY = Math.floor(model.max.y.get() + 1.5);
+
         maxX = Math.ceil(maxX / step) * step;
         maxY = Math.ceil(maxY / step) * step;
+
+        // Previous
+
+        if (step == previousStep && minX == previousMinX && minY == previousMinY && maxX == previousMaxX && maxY == previousMaxY) {
+            return;
+        } else {
+            getChildren().clear();
+        }
+
+        previousStep = step;
+
+        previousMinX = minX;
+        previousMinY = minY;
+
+        previousMaxX = maxX;
+        previousMaxY = maxY;
+
+        // Lines
 
         for (double x = minX; x <= maxX; x += step) {
             Color color = Math.round(x / step) % 100 == 0 ? Color.BLACK : (Math.round(x / step) % 10 == 0 ? Color.GRAY : Color.LIGHTGRAY);
