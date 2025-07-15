628ecdf772ce8a3499ce00294c39b9f5b1f04043
TobiasSfhooe <s2210564021@fhooe.at>
2025-07-15 22:06:24 +0200

    added binding to station rectangle

--- modules/fx/src/main/java/io/github/ghackenberg/mbse/transport/fx/viewers/flat/StationViewerFlat.java
+++ modules/fx/src/main/java/io/github/ghackenberg/mbse/transport/fx/viewers/flat/StationViewerFlat.java
@@ -4,8 +4,8 @@ import io.github.ghackenberg.mbse.transport.core.Model;
 import io.github.ghackenberg.mbse.transport.core.entities.Station;
 import io.github.ghackenberg.mbse.transport.core.structures.Vector;
 import io.github.ghackenberg.mbse.transport.fx.viewers.StationViewer;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.beans.binding.Bindings;
 
 public class StationViewerFlat extends StationViewer {
 	
@@ -13,9 +13,6 @@ public class StationViewerFlat extends StationViewer {
 	public final Rectangle rectangle;
 	public final double width = 1.0;
 	public final double height = 1.0;
-	private double normalX;
-	private double normalY;
-	private double angle;
 
 	public StationViewerFlat(Model model, Station station) {
 		super(model, station);
@@ -31,19 +28,23 @@ public class StationViewerFlat extends StationViewer {
 		rectangle.setHeight(height);
 		rectangle.xProperty().bind(location.x.subtract(width / 2));
 		rectangle.yProperty().bind(location.y.subtract(height / 2));
+		//rectangle.rotateProperty().bind(location.x.multiply(10).add(location.y.multiply(0)));
+		//rectangle.rotateProperty().bind(new SimpleDoubleProperty(1).multiply(location.x.multiply(10).add(location.y.multiply(0))));
+		//rectangle.rotateProperty().bind(new SimpleDoubleProperty(1).multiply(location.x.multiply(10).add(location.y.multiply(0))));
+		rectangle.rotateProperty().bind(
+			    Bindings.createDoubleBinding(
+			        () -> Math.toDegrees(Math.atan2(station.location.segment.get().tangent.y.get(), station.location.segment.get().tangent.x.get())),
+			        location.x, location.y
+			    )
+			);
 		
-		// get angle from segment tangent normal
-		normalX = station.location.segment.get().tangentNormal.x.get();
-		normalY = station.location.segment.get().tangentNormal.y.get();
-		angle = Math.atan2(normalY, normalX);
+		//rectangle.rotateProperty().bind(new SimpleDoubleProperty( Math.atan2(station.location.segment.get().tangent.y.get(), station.location.segment.get().tangent.x.get())));
 
-		// rotate rectangle to match segment tangent normal
-		rectangle.setRotate(Math.toDegrees(angle));
 		
 		rectangle.fillProperty().bind(color);
 		
-		// Circle
-		
+//		// Circle
+//		
 //		circle = new Circle();
 //		
 //		circle.setRadius(0.5);
